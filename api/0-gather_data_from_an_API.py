772eee5fe3ab945b102d7036041be24b4af93225
Dorcas <tdorcas-akim>
2025-07-15 02:30:11 -0700

    task 1

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -23,7 +23,6 @@ if __name__ == "__main__":
 
     # Base URL for the API
     base_url = "https://jsonplaceholder.typicode.com/"
-
     # --- Step 1: Get Employee Information ---
     # We will send a request to get the user's details, like their name."  
     # The URL wil be like: https://jsonplaceholder.typicode.com/users/2"
